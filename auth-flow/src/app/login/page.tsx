a2d05b791009fdf81f81075ab97010cbbc8f2f24
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-01-18 21:37:32 +0530

    feat: implement login API and enhance login page with loading state and validation

--- auth-flow/src/app/login/page.tsx
+++ auth-flow/src/app/login/page.tsx
@@ -1,21 +1,50 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios  from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+
+  const router = useRouter();
+
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+   const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("login success", response.data);
+      toast.success("login success");
+      router.push("/profile");
+      
+    } catch (error : any) {
+      console.log( "login failed",error.message);
+      toast.error(error.message);
+    }finally{
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length  > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="p-4 font-bold text-2xl">Login</h1>
+      <h1 className="p-4 font-bold text-2xl">{loading ? "Processing" : "Logging"}</h1>
       <label htmlFor="email">Email</label>
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none
@@ -39,7 +68,7 @@ export default function LoginPage() {
       <button
         onClick={onLogin}
         className="p-2 bg-white border border-gray-300 rounded-lg mb-4 focus:outline-nonefocus:border-gray-600 text-blue-700 font-bold">
-        Login </button>
+        {buttonDisabled ? "Can't Login" : "Login"} </button>
         <Link href='/signup'>Using app for first time Click to Signup</Link>
     </div>
   );
