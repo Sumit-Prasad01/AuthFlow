e83a36670f2e6893f7ebe28055c003cfc11cf183
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-01-19 16:57:50 +0530

    feat: enhance user profile page with user details retrieval and improve error handling

--- auth-flow/src/app/api/users/me/route.ts
+++ auth-flow/src/app/api/users/me/route.ts
@@ -1,22 +1,21 @@
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
-import user from "@/models/userModel";
+import User from "@/models/userModel";
 import { connect } from "@/dbConfig/dbConfig";
-import next from "next";
 
 connect();
 
-export async function GET() {
-  try {
-    const userID = await getDataFromToken(request);
-    const user = await UserActivation.findOne({ _id: userID }).select(
-      "-password "
-    );
-    return NextResponse.json({
-      message: "User Found",
-      data: user,
-    });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+export async function GET(request:NextRequest){
+
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password");
+        return NextResponse.json({
+            mesaaage: "User found",
+            data: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+
 }
--- auth-flow/src/app/profile/page.tsx
+++ auth-flow/src/app/profile/page.tsx
@@ -1,31 +1,50 @@
 "use client";
 
+import React, {useState} from "react";
 import axios from "axios";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-export default function ProfilePage(){
-    const router = useRouter();
-    const logout = async () => {
-        try {
-            await axios.get("/api/users/logout");
-            toast.success("User logged out successfully");
-            router.push("/login");
-        } catch (error : any) {
-            console.log(error.message);
-            toast.error(error.message);
-        }
-    }
 
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <p>Profile page</p>
-            <hr />
-            <button onClick={logout}
-             className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-4 rounded-lg">Logout</button>
-        </div>
-    )
+export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState('nothing');  
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("User logged out successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    setData(res.data.data._id);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <h2 className="m-4 p-2 rounded bg-white text-slate-900 ">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-4 rounded-lg"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-blue-500 hover:bg-green-600 text-white font-bold py-2 px-4 m-4 rounded-lg"
+      >
+        Get user details
+      </button>
+    </div>
+  );
 }
